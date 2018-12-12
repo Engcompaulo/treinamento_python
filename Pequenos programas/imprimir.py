c6863665eeefe6baf6cb034f76a528566935cb19
Engcompaulo <engcompaulo@gmail.com>
2018-12-12 15:59:41 -0200

    Set

--- Pequenos programas/imprimir.py
+++ Pequenos programas/imprimir.py
@@ -18,3 +18,28 @@ largura = 45
 area = altura * largura
 msg = 'A altura mede {} m e a largura mede {} m, com isso a área é {} m².'
 print(msg.format(altura, largura, area))
+
+
+mes = {
+  'janeiro': 31,
+  'fevereiro': 27,
+  'março': 31,
+  'abril':30,
+  'maio':31,
+  'junho':30,
+  'julho': 31,
+  'agosto':31,
+  'setembro':30,
+  'outubro':31,
+  'novembro':30,
+  'dezembro':31
+}
+print(mes)
+print(sorted(mes))
+print('\n')
+print('\n'.join(mes))
+print(mes['abril'])
+mesNome = sorted(mes)[0]
+mesDia = mes['abril']
+msg2 = 'O mês {} tem {} dias'
+print(msg2.format(mesNome, mesDia))
